4e4437ec4109a716d61061e4bb2312030164054b
developerDesinger <deisgnercoo@gmail.com>
2026-03-30 14:18:39 +0500

    Adding Frontend

---
+++ Backend/src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+import connectDb from "./db/index.js";
+import app from "./app.js";
+
+app.on("error", (error) => {
+    console.log("Error in connection", error);
+    throw error;
+});
+
+connectDb()
+.then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 5000}`);
+    });
+})
+.catch((error) => {
+    console.log("Connection not Established", error);
+});
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,88 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+
+    },
+    avatar: {
+        type: String, // we will store the images on Cloudinary and get the Url from there.
+        required: true,
+
+    },
+    coverImage: {
+        type: String, // we will store the images on Cloudinary and get the Url from there.
+
+    },
+    watchHistory: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video",
+        },
+    ],
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters Long"],
+
+
+    },
+    refreshToken: {
+        type: String,
+
+
+    },
+
+
+
+}, {
+
+    timestamps: true,
+})
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        userId: this.id,
+        email: this.email,
+        username: this.username
+    }, process.env.JWT_SECRET, { expiresIn: process.env.Access_Token_Expire_Time })
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        userId: this.id,
+    }, process.env.Refresh_Token_Secret, { expiresIn: process.env.Refresh_Token_Expire_Time })
+}
+
+
+export const User = mongoose.model("User", userSchema)
+
+
